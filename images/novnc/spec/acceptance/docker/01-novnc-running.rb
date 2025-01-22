87cd51de03f5170238b7d14e2628101b7f753a7c
Charlie Sharpsteen <charles@sharpsteen.dev>
2025-01-22 00:42:29 +0000

    Enable supervisorctl in novnc image
    This commit updates the `supervisord.conf` file used by the noVNC
    image to include configuration required to enable use of the
    `supervisorctl` tool. This is useful for re-starting processes
    or accessing process stdout.

--- images/novnc/spec/acceptance/docker/01-novnc-running.rb
+++ images/novnc/spec/acceptance/docker/01-novnc-running.rb
@@ -5,6 +5,10 @@ context 'noVNC Installation' do
     set :docker_container, 'novnc-test'
   end
 
+  describe command('supervisorctl avail') do
+    its(:exit_status) { should eq 0 }
+  end
+
   describe port(8000) do
     pending "SupervisorD takes a while to spin up, there is a race condition to solve here."
     it { should be_listening }
